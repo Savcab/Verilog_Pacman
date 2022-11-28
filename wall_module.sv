2c062a0692f926e552400a30086d1b4a864b5d02
Savcab <felixchenyijun@gmail.com>
2022-11-28 15:48:54 -0800

    Added vertical componeents to wall_components

--- wall_module.sv
+++ wall_module.sv
@@ -19,39 +19,41 @@ OFFSETH2 = 130,
 WALLCOLOR = 12'b0000_0000_1111;
 
 // function for making checking teh boundaries easier
-function bit inBorder;
-    input [9:0] x, y, xMin, yMin, xMax, yMax;
-    output inbound;
-    inbound = (x >= xMin && x <= xMax && y >= yMin && y <= yMax);
+//function bit inBorder;
+//    input [9:0] x, y, xMin, yMin, xMax, yMax;
+//    output inbound;
+//    inBorder = (x >= xMin && x <= xMax && y >= yMin && y <= yMax);
+//endfunction
+
+function bit inBorder (int x, int y, int xMin, int yMin, int xMax, int yMax);
+    return (x >= xMin && x <= xMax && y >= yMin && y <= yMax);
 endfunction
 
+wire compA, compB, compC, compD, compE, compF, compG, compH, compI, compJ, compK, compL, compM, compN, compO, compP, compQ, compR, compS;
 
-assign compA = inBorder(hCount, vCount, );
-assign compB = inBorder(hCount, vCount, );
-assign compC = inBorder(hCount, vCount, );
-assign compD = inBorder(hCount, vCount, );
-assign compE = inBorder(hCount, vCount, );
-assign compF = inBorder(hCount, vCount, );
-assign compG = inBorder(hCount, vCount, );
-assign compH = inBorder(hCount, vCount, );
-assign compI = inBorder(hCount, vCount, );
-assign compJ = inBorder(hCount, vCount, );
-assign compK = inBorder(hCount, vCount, );
-assign compL = inBorder(hCount, vCount, );
-assign compM = inBorder(hCount, vCount, );
-assign compO = inBorder(hCount, vCount, );
-assign compP = inBorder(hCount, vCount, );
-assign compQ = inBorder(hCount, vCount, );
-assign compR = inBorder(hCount, vCount, );
-assign compS = inBorder(hCount, vCount, );
-
-
-
+assign compA = inBorder(hCount, vCount, OFFSETH1 + 144, OFFSETV1 + 8, OFFSETH2 + 156, OFFSETV2 + 48);
+assign compB = inBorder(hCount, vCount, OFFSETH1 + 224, OFFSETV1 + 8, OFFSETH2 + 236, OFFSETV2 + 48);
+assign compC = inBorder(hCount, vCount, OFFSETH1 + 104, OFFSETV1 + 36, OFFSETH2 + 116, OFFSETV2 + 88);
+assign compD = inBorder(hCount, vCount, OFFSETH1 + 264, OFFSETV1 + 36, OFFSETH2 + 276, OFFSETV2 + 88);
+assign compE = inBorder(hCount, vCount, OFFSETH1 + 184, OFFSETV1 + 36, OFFSETH2 + 196, OFFSETV2 + 128);
+assign compF = inBorder(hCount, vCount, OFFSETH1 + 36, OFFSETV1 + 88, OFFSETH2 + 48, OFFSETV2 + 128);
+assign compG = inBorder(hCount, vCount, OFFSETH1 + 332, OFFSETV1 + 88, OFFSETH2 + 344, OFFSETV2 + 128);
+assign compH = inBorder(hCount, vCount, OFFSETH1 + 104, OFFSETV1 + 156, OFFSETH2 + 116, OFFSETV2 + 264);
+assign compI = inBorder(hCount, vCount, OFFSETH1 + 264, OFFSETV1 + 156, OFFSETH2 + 276, OFFSETV2 + 264);
+assign compJ = inBorder(hCount, vCount, OFFSETH1 + 64, OFFSETV1 + 236, OFFSETH2 + 76, OFFSETV2 + 304);
+assign compK = inBorder(hCount, vCount, OFFSETH1 + 304, OFFSETV1 + 236, OFFSETH2 + 316, OFFSETV2 + 304);
+assign compL = inBorder(hCount, vCount, OFFSETH1 + 184, OFFSETV1 + 252, OFFSETH2 + 196, OFFSETV2 + 304);
+assign compM = inBorder(hCount, vCount, OFFSETH1 + 104, OFFSETV1 + 292, OFFSETH2 + 116, OFFSETV2 + 356);
+assign compN = inBorder(hCount, vCount, OFFSETH1 + 264, OFFSETV1 + 292, OFFSETH2 + 276, OFFSETV2 + 356);
+assign compO = inBorder(hCount, vCount, OFFSETH1 + 36, OFFSETV1 + 344, OFFSETH2 + 48, OFFSETV2 + 384);
+assign compP = inBorder(hCount, vCount, OFFSETH1 + 144, OFFSETV1 + 332, OFFSETH2 + 156, OFFSETV2 + 384);
+assign compQ = inBorder(hCount, vCount, OFFSETH1 + 224, OFFSETV1 + 332, OFFSETH2 + 236, OFFSETV2 + 384);
+assign compR = inBorder(hCount, vCount, OFFSETH1 + 332, OFFSETV1 + 344, OFFSETH2 + 344, OFFSETV2 + 384);
+assign compS = inBorder(hCount, vCount, OFFSETH1 + 184, OFFSETV1 + 332, OFFSETH2 + 196, OFFSETV2 + 356);
 
 
 //	for coloring wall
-assign wallFill = inBorder(hCount, vCount, )
-                    || 
+assign wallFill = compA || compB || compC || compD || compE|| compF || compG || compH || compI || compJ || compK || compL || compM || compN || compO || compP || compQ || compR || compS;
 
 
 // for coloring
